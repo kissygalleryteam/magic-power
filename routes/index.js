7c97d108023ba3f6a74b6c9ad0c3d78f6ed8dbce
伯方 <bofang.zxj@taobao.com>
2014-05-07 21:05:53 -0700

    update

--- routes/index.js
+++ routes/index.js
@@ -3,30 +3,12 @@ var DB = require('../lib/db');
 
 
 
-/**
- * repos api
- */
 
-// DB.repos.add({
-// 	name:'uploader',
-// 	user:'jianping',
-// 	tag:'form',
-// 	desc:'upload',
-// 	cover:'xxx.jpg',
-// 	version:'1.5'
-// })
-// DB.repos.getAll()
-// 	.then(function(d){
-// 		console.log(d);
-// 	});
-
-DB.repos.queryByKeyword('up')
-	.then(function(d){
-		console.log(d);
-	})
-
-
-// DB.repos.getByName('offline')
-// 	.then(function(d){
-// 		console.log(d);
-// 	})
+DB.repos.add({
+	name:'imagefilter',
+	user:'zihan',
+	tag:'img'
+})
+.then(function(d){
+	console.log(d);
+})
